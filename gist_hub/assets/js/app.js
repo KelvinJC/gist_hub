1b7344d113944fc35bb5a61922eaaf51eb5d213e
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-14 15:20:30 +0100

    Fix scrolling disparity between textarea and line numbers with a JS event listener

--- gist_hub/assets/js/app.js
+++ gist_hub/assets/js/app.js
@@ -31,6 +31,11 @@ Hooks.UpdateLineNumbers = {
       this.updateLineNumbers()
     })
 
+    this.el.addEventListener("scroll", () => {
+      const lineNumberText = document.querySelector("#line-numbers")
+      lineNumberText.scrollTop = this.el.scrollTop
+    })
+
     this.updateLineNumbers()
   },
   updateLineNumbers() {

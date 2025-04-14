3c4f934d69d954b30d29bf1d44caa0c7caad6cb8
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-14 15:58:04 +0100

    Define event handler for clearing textarea field

--- gist_hub/assets/js/app.js
+++ gist_hub/assets/js/app.js
@@ -27,17 +27,24 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let Hooks = {};
 Hooks.UpdateLineNumbers = {
   mounted() {
+    const lineNumberText = document.querySelector("#line-numbers")
+
     this.el.addEventListener("input", () => {
       this.updateLineNumbers()
     })
 
     this.el.addEventListener("scroll", () => {
-      const lineNumberText = document.querySelector("#line-numbers")
       lineNumberText.scrollTop = this.el.scrollTop
+    })
+
+    this.handleEvent("clear-textarea", () => {
+      this.el.value = ""
+      lineNumberText.value = "1\n"
     })
 
     this.updateLineNumbers()
   },
+
   updateLineNumbers() {
     const lineNumberText = document.querySelector("#line-numbers")
     if (!lineNumberText) return;

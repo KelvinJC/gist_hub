733a464dc17a6e4564e0a19505bef706bac468f4
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-14 09:57:26 +0100

    Specify cardinality between users and gists

--- gist_hub/lib/gist_hub/accounts/user.ex
+++ gist_hub/lib/gist_hub/accounts/user.ex
@@ -9,6 +9,7 @@ defmodule GistHub.Accounts.User do
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
     field :confirmed_at, :utc_datetime
+    has_many :gists, GistHub.Gists.Gist
 
     timestamps(type: :utc_datetime)
   end

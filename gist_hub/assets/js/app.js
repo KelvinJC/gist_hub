e3457334d66209ce20524ab077010891bff51b2a
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-15 10:37:32 +0100

    Include dependency for syntax highlighting

--- gist_hub/assets/js/app.js
+++ gist_hub/assets/js/app.js
@@ -22,6 +22,9 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+// include syntax highlight dependency
+import hljs from "highlight.js"
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 let Hooks = {};

e7ea7cf004de3affe053475b1f0a81aaab5faf6b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-16 17:48:40 +0100

    Restrict gist deletion to author

--- gist_hub/lib/gist_hub/gists.ex
+++ gist_hub/lib/gist_hub/gists.ex
@@ -86,8 +86,15 @@ defmodule GistHub.Gists do
       {:error, %Ecto.Changeset{}}
 
   """
-  def delete_gist(%Gist{} = gist) do
-    Repo.delete(gist)
+  def delete_gist(%User{} = user, gist_id) do
+    gist = Repo.get!(Gist, gist_id)
+
+    if user.id == gist.user_id do
+      Repo.delete(gist)
+      {:ok, gist}
+    else
+      {:error, :unauthorised}
+    end
   end
 
   @doc """

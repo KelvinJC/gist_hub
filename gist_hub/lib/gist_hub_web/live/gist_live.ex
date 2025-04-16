78909f4d4cfb367700c391b2fa3706dc5ce84e45
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-16 18:17:35 +0100

    Define callback to handle delete events

--- gist_hub/lib/gist_hub_web/live/gist_live.ex
+++ gist_hub/lib/gist_hub_web/live/gist_live.ex
@@ -6,4 +6,20 @@ defmodule GistHubWeb.GistLive do
     gist = Gists.get_gist!(id)
     {:ok, assign(socket, gist: gist)}
   end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    case Gists.delete_gist(socket.assigns.current_user, id) do
+      {:ok, _gist} ->
+        socket = put_flash(socket, :info, "Gist successfully deleted.")
+        {:noreply, push_navigate(socket, to: ~p"/create")}
+      {:error, :unauthorised} ->
+        socket = put_flash(socket, :error, "Unauthorised.")
+        {:noreply, socket}
+      {:error, message} ->
+        socket = put_flash(socket, :error, message)
+        {:noreply, socket}
+      _ ->
+      {:noreply, socket}
+    end
+  end
 end

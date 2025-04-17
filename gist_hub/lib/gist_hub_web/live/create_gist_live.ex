a5117ecafd241f83ef19ca0eb46a5f89d44a3541
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-17 22:10:19 +0100

    Replace heex template with render/1 function in GistHubWeb.CreateGistLive module

--- gist_hub/lib/gist_hub_web/live/create_gist_live.ex
+++ gist_hub/lib/gist_hub_web/live/create_gist_live.ex
@@ -6,4 +6,19 @@ defmodule GistHubWeb.CreateGistLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  def render(assigns) do
+    ~H"""
+      <div class="gh-gradient flex items-center justify-center">
+        <h1 class="font-brand font-bold text-white text-3xl">
+            Instantly share Elixir code, notes and snippets.
+        </h1>
+      </div>
+      <.live_component
+        module={GistFormComponent}
+        form={to_form(Gists.change_gist(%Gist{}))}
+        id={:new} current_user={@current_user}
+      />
+    """
+  end
 end

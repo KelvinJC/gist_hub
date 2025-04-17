bb3e3c748a20fbe45c6e0d825276b30e6dd0ff8b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-17 21:21:02 +0100

    Alias required modules for form attribute definition in heex template

--- gist_hub/lib/gist_hub_web/live/create_gist_live.ex
+++ gist_hub/lib/gist_hub_web/live/create_gist_live.ex
@@ -1,6 +1,7 @@
 defmodule GistHubWeb.CreateGistLive do
   use GistHubWeb, :live_view
   alias GistHubWeb.GistFormComponent
+  alias GistHub.{Gists, Gists.Gist}
 
   def mount(_params, _session, socket) do
     {:ok, socket}

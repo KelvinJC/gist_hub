f758c7a2c042611365daa5333f032d180c27ca14
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-18 13:43:22 +0100

    Redefine callback handling gist creation to support gist update as well

--- gist_hub/lib/gist_hub_web/live/gist_form_component.ex
+++ gist_hub/lib/gist_hub_web/live/gist_form_component.ex
@@ -62,7 +62,23 @@ defmodule GistHubWeb.GistFormComponent do
     """
   end
 
+  def handle_event("validate", %{"gist" => params}, socket) do
+    changeset =
+      %Gist{}
+      |> Gists.change_gist(params)
+      |> Map.put(:action, :validate)
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+
   def handle_event("create", %{"gist" => params}, socket) do
+    if socket.assigns.id == :new do
+      create_gist(params, socket)
+    else
+      update_gist(params, socket)
+    end
+  end
+
+  defp create_gist(params, socket) do
     case Gists.create_gist(socket.assigns.current_user, params) do
       {:ok, gist} ->
         # trigger client side event on successful creation of gist
@@ -75,11 +91,22 @@ defmodule GistHubWeb.GistFormComponent do
     end
   end
 
-  def handle_event("validate", %{"gist" => params}, socket) do
-    changeset =
-      %Gist{}
-      |> Gists.change_gist(params)
-      |> Map.put(:action, :validate)
-    {:noreply, assign(socket, :form, to_form(changeset))}
+  defp update_gist(params, socket) do
+    params = Map.put(params, "id", socket.assigns.id)
+    case Gists.update_gist(socket.assigns.current_user, params) do
+      {:ok, gist} ->
+        # trigger client side event on successful update of gist
+        socket = push_event(socket, "clear-textarea", %{})
+        changeset = Gists.change_gist(%Gist{})
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist]}")}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+      {:error, :unauthorised} ->
+        socket = put_flash(socket, :error, "Unauthorised.")
+        {:noreply, socket}
+      _ ->
+        {:noreply, socket}
+    end
   end
 end

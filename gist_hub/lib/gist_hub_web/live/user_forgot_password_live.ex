2ff62384d5496ece3f0bb9a2976a7608ba7a958a
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-22 16:12:37 +0100

    Polish UI for password reset live view

--- gist_hub/lib/gist_hub_web/live/user_forgot_password_live.ex
+++ gist_hub/lib/gist_hub_web/live/user_forgot_password_live.ex
@@ -5,23 +5,45 @@ defmodule GistHubWeb.UserForgotPasswordLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
+    <div class="gh-gradient flex flex-col items-center justify-center">
+      <h1 class="font-brand font-bold text-3xl text-white py-4">
         Forgot your password?
-        <:subtitle>We'll send a password reset link to your inbox</:subtitle>
-      </.header>
-
-      <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
-        <.input field={@form[:email]} type="email" placeholder="Email" required />
-        <:actions>
-          <.button phx-disable-with="Sending..." class="w-full">
-            Send password reset instructions
-          </.button>
-        </:actions>
-      </.simple_form>
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+      </h1>
+      <h2 class="font-brand font-regular text-sm text-ghDark-light">
+        We'll send a password reset link to your inbox
+      </h2>
+    </div>
+    <div class="mx-auto max-w-sm space-y-12">
+      <.form for={@form} id="reset_password_form" phx-submit="send_email">
+        <div class="justify-center w-full mb-10">
+          <.input 
+            field={@form[:email]} 
+            type="email" 
+            placeholder="Email" 
+            required 
+          />
+        </div>
+        <.button 
+          phx-disable-with="Sending..." 
+          class="create_button w-full"
+          >
+          Send password reset instructions
+        </.button>
+      </.form>
+      <p class="font-brand font-regular text-center text-sm mt-4 text-ghDark-light">
+        <.link 
+          class="hover:underline hover:text-ghLavender-dark mr-2"
+          href={~p"/users/register"}
+        >
+          Register
+        </.link>
+        | 
+        <.link
+        class="hover:underline hover:text-ghLavender-dark ml-2" 
+          href={~p"/users/log_in"}
+        >
+          Log in
+        </.link>
       </p>
     </div>
     """

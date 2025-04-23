ec3b2725a5ce0fc3f253edc29ea2910e2c9b1d16
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-23 13:13:10 +0100

    Use index value to ensure unique ids of textarea element

--- gist_hub/lib/gist_hub_web/live/all_gists_live.ex
+++ gist_hub/lib/gist_hub_web/live/all_gists_live.ex
@@ -81,8 +81,8 @@ defmodule GistHubWeb.AllGistsLive do
         </div>
       </div>
         <div id="gist-wrapper" class="flex w-full" >
-          <textarea id="syntax-line-numbers" class="all-syntax-numbers rounded-bl-md" readonly></textarea>
-          <div id="highlight" class="all-syntax-area w-full rounded-br-md" phx-hook="Highlight" data-name={@gist.name} all-gists-flag="true">
+          <textarea id={"syntax-line-numbers-#{@index}"} class="all-syntax-numbers rounded-bl-md" readonly></textarea>
+          <div id="highlight" class="all-syntax-area w-full rounded-br-md" phx-hook="Highlight" data-name={@gist.name} gist-index={@index}>
             <pre><code class="language-elixir"><%= get_preview_text(@gist.markup_text) %></code></pre>
           </div>
         </div>

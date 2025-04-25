cc68bd6a3907a6659ebd6a106f7b971689ec268b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-25 15:38:56 +0100

    Add button icons for password toggle

--- gist_hub/lib/gist_hub_web/live/user_reset_password_live.ex
+++ gist_hub/lib/gist_hub_web/live/user_reset_password_live.ex
@@ -19,30 +19,60 @@ defmodule GistHubWeb.UserResetPasswordLive do
           Oops, something went wrong! Please check the errors below.
         </.error>
         <div class="justify-center space-y-8 w-full mb-10">
-          <.input 
-          field={@form[:password]} 
-          type="password" 
-          placeholder="New password" 
-          required 
-          />
-          <.input
-            field={@form[:password_confirmation]}
-            type="password"
-            placeholder="Confirm new password"
-            required
-          />
+          <div id="new_pwd_wrapper" class="flex relative">
+            <div class="w-full">
+              <.input
+                field={@form[:password]}
+                type="password"
+                placeholder="New password"
+                required
+              />
+            </div>
+            <button id="hide_new_pwd" class="absolute right-0 mt-3 py-2 px-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+              </svg>
+            </button>
+            <button id="show_new_pwd" class="hidden absolute right-0 mt-3 py-2 px-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            </button>
+          </div>
+          <div id="confirm_pwd_wrapper" class="flex relative">
+            <div class="w-full">
+              <.input
+                field={@form[:password_confirmation]}
+                type="password"
+                placeholder="Confirm new password"
+                required
+              />
+            </div>
+            <button id="hide_confirm_pwd" class="absolute right-0 mt-3 py-2 px-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+              </svg>
+            </button>
+            <button id="show_confirm_pwd" class="hidden absolute right-0 mt-3 py-2 px-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            </button>
+          </div>
         </div>
-        <.button phx-disable-with="Resetting..." class="create_button w-full">Reset Password</.button>
+          <.button phx-disable-with="Resetting..." class="create_button w-full">Reset Password</.button>
       </.form>
       <p class="font-brand font-regular text-center text-sm text-ghDark-light mt-4">
-        <.link 
+        <.link
           class="hover:underline hover:text-ghLavender-dark mr-2"
           href={~p"/users/register"}
         >
           Register
         </.link>
-        | 
-        <.link 
+        |
+        <.link
           class="hover:underline hover:text-ghLavender-dark ml-2"
           href={~p"/users/log_in"}
         >

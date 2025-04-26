3e50260ca92cf3eb465d557f1c898968172ce89b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-26 19:38:27 +0100

    Implement server-side-controlled toggle of password visibility in sign up form

--- gist_hub/lib/gist_hub_web/live/user_registration_live.ex
+++ gist_hub/lib/gist_hub_web/live/user_registration_live.ex
@@ -21,7 +21,6 @@ defmodule GistHubWeb.UserRegistrationLive do
       </h2>
     </div>
     <div class="mx-auto max-w-sm">
-
       <.form
         for={@form}
         id="registration_form"
@@ -41,12 +40,34 @@ defmodule GistHubWeb.UserRegistrationLive do
           placeholder="Email"
           required
           />
-          <.input
-            field={@form[:password]}
-            type="password"
-            placeholder="Password"
-            required
-          />
+          <div class="flex relative">
+            <div class="w-full">
+              <.input
+                id="pwd_field"
+                field={@form[:password]}
+                type={@show_password && "text" || "password"}
+                placeholder="Password"
+                value={@pwd_field || ""}
+                required
+              />
+            </div>
+            <%= if @pwd_field_has_text do %>
+              <div
+                id="toggle_pwd"
+                class="absolute right-3 mt-5 cursor-pointer"
+                phx-click="toggle"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-4 toggle-pwd-icon">
+                  <%= if @show_password do %>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  <% else %>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  <% end %>
+                </svg>
+              </div>
+            <%= end %>
+          </div>
           <div class="flex justify-end">
             <.button
               class="create_button w-full"
@@ -66,7 +87,12 @@ defmodule GistHubWeb.UserRegistrationLive do
 
     socket =
       socket
-      |> assign(trigger_submit: false, check_errors: false)
+      |> assign(
+        trigger_submit: false,
+        check_errors: false,
+        show_password: false,
+        pwd_field_has_text: false, pwd_field: ""
+      )
       |> assign_form(changeset)
 
     {:ok, socket, temporary_assigns: [form: nil]}
@@ -90,8 +116,18 @@ defmodule GistHubWeb.UserRegistrationLive do
   end
 
   def handle_event("validate", %{"user" => user_params}, socket) do
+    socket = assign(
+      socket,
+      pwd_field: user_params["password"],
+      pwd_field_has_text: String.length(user_params["password"] ) > 0
+    )
     changeset = Accounts.change_user_registration(%User{}, user_params)
     {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
+  end
+
+  def handle_event("toggle", _, socket) do
+    socket = assign(socket, :show_password, !socket.assigns.show_password)
+    {:noreply, socket}
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do

04bb2176487e248934e5dce4fc6ce87595efb162
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-04-30 09:23:11 +0100

    Format class attributes

--- gist_hub/lib/gist_hub_web/live/search_bar_component.ex
+++ gist_hub/lib/gist_hub_web/live/search_bar_component.ex
@@ -14,9 +14,8 @@ defmodule GistHubWeb.SearchBarComponent do
         <div class="flex mr-2">
           <input
             type="text"
-            class="rounded-l-lg bg-ghDark-dark border-white py-1
-              focus:outline-none focus:border-ghLavender focus:ring-0
-              placeholder-ghDark-light text-white text-sm font-brand font-regular "
+            class="rounded-l-lg bg-ghDark-dark border-white py-1 focus:outline-none focus:border-ghLavender
+                  focus:ring-0 placeholder-ghDark-light text-white text-sm font-brand font-regular"
             placeholder="Search..."
             id={@form[:search_term].id}
             name={@form[:search_term].name}
@@ -28,7 +27,11 @@ defmodule GistHubWeb.SearchBarComponent do
               focus:outline-none focus:border-ghLavender focus:ring-0"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="gray" class="size-4">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
             </svg>
           </button>
         </div>

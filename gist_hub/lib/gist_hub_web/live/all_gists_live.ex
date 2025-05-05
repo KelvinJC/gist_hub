38472771976a043da653a570ffab1618f60dc237
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-05-05 11:17:40 +0100

    Replace comment count and saved count image icons with Heroicons

--- gist_hub/lib/gist_hub_web/live/all_gists_live.ex
+++ gist_hub/lib/gist_hub_web/live/all_gists_live.ex
@@ -87,11 +87,31 @@ defmodule GistHubWeb.AllGistsLive do
           </div>
         </div>
         <div class="mr-8 px-3">
-          <div class="flex items-center w-4">
-            <img src="/images/comment.svg" alt="Comment Count">
-            <span class="text-white h-6 px-1">0</span>
-            <img src="/images/BookmarkOutline.svg" alt="Save Count">
-            <span class="text-white h-6 px-1">0</span>
+          <div class="flex items-center w-4 mr-2">
+            <button class="flex items-center mr-2" alt="Comment Count">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="gray"
+                class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+              </svg>
+              <span class="text-ghDark-light h-6 px-1">0</span>
+            </button>
+            <button class="flex items-center" alt="Comment Count">
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="gray" 
+                class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+              </svg>
+              <span class="text-ghDark-light h-6 px-1">0</span>
+            </button>
           </div>
         </div>
       </div>

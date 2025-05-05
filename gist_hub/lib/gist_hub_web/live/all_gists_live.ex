5f471f50288d74f39ec567f9b01c87f4d3c9bf87
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-05-05 17:36:51 +0100

    Add view count icon

--- gist_hub/lib/gist_hub_web/live/all_gists_live.ex
+++ gist_hub/lib/gist_hub_web/live/all_gists_live.ex
@@ -86,8 +86,23 @@ defmodule GistHubWeb.AllGistsLive do
             </p>
           </div>
         </div>
-        <div class="px-3">
+        <div>
           <div class="flex items-center">
+            <button class="mr-2" alt="View Count">
+                <div class="flex justify-center items-center">
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    stroke-width="1.5" 
+                    stroke="gray" 
+                    class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  </svg>
+                  <span class="text-ghDark-light text-xs h-5 px-1">0 views</span>  
+                </div>
+            </button>
             <button class="mr-2" alt="Comment Count">
                 <div class="flex justify-center items-center">
                   <svg
@@ -102,7 +117,7 @@ defmodule GistHubWeb.AllGistsLive do
                   <span class="text-ghDark-light text-xs h-5 px-1">0 comments</span>  
                 </div>
             </button>
-            <button class="mr-2" alt="Saves Count">
+            <button alt="Save Count">
               <div class="flex justify-center items-center">
                 <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -114,7 +129,7 @@ defmodule GistHubWeb.AllGistsLive do
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
               </svg>
               <span class="text-ghDark-light text-xs h-5 px-1">0 saves</span>              </div>
-          </button>
+            </button>
           </div>
         </div>
       </div>

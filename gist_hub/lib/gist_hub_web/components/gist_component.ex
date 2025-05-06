5600bd0178512e67b583ab4263bb5dde19e25812
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2025-05-06 12:05:43 +0100

    Define gist component

---
+++ gist_hub/lib/gist_hub_web/components/gist_component.ex
@@ -0,0 +1,103 @@
+defmodule GistHubWeb.GistComponent do
+  use Phoenix.Component
+  alias GistHubWeb.Utils.{DateFormat, FormatUsername, FormatMarkup}
+
+  attr :gist_path, :string, required: true, doc: "path to gist"
+
+  def gist(assigns) do
+    ~H"""
+    <div class="justify-center px-20 w-full mb-20">
+      <div class="flex justify-between mb-2">
+        <div class="flex items-center">
+          <img
+            src="/images/user-image.svg"
+            alt="Profile Image"
+            class="rounded-full w-6 h-6"
+          >
+          <div class="flex flex-col ml-4">
+            <div class="text-base font-brand text-sm">
+              <span class="text-white"><%= FormatUsername.strip_name_from_email(@gist.user.email) %></span>
+              <span class="font-bold text-white">/</span>
+              <.link
+                href= {@gist_path}
+                class="text-ghLavender-dark hover:underline"
+                >
+                <%= @gist.name %>
+              </.link>
+            </div>
+            <div class="font-brand text-ghDark-light text-xs">
+              <%=
+               @sort_by_updated_at
+               && "Last active " <> DateFormat.get_relative_time(@gist.updated_at)
+               || "Created " <> DateFormat.get_relative_time(@gist.inserted_at)
+              %>
+            </div>
+            <p class="text-ghDark-light text-xs">
+              <%= @gist.description %>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center px-1">
+            <button class="mr-2" alt="View Count">
+              <div class="flex justify-center items-center space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="gray"
+                  class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                <span class="text-ghDark-light text-xs h-4 ">{@gist.views} {@gist.views == 1 && "view" || "views"}</span>
+              </div>
+            </button>
+            <button class="mr-2" alt="Comment Count" >
+              <div class="flex justify-center items-center space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="gray"
+                  class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                </svg>
+                <span class="text-ghDark-light text-xs h-4">0 comments</span>
+              </div>
+            </button>
+            <button alt="Save Count">
+              <div class="flex justify-center items-center  space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="gray"
+                  class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
+                <span class="text-ghDark-light text-xs h-4">0 saves</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+        <div id={"gist-wrapper-#{@index}"} class="flex w-full" >
+          <textarea id={"syntax-line-numbers-#{@index}"} class="all-syntax-numbers rounded-bl-md" readonly></textarea>
+          <div
+            id={"highlight-#{@index}"}
+            class="all-syntax-area w-full rounded-br-md"
+            phx-hook="Highlight"
+            data-name={@gist.name}
+            gist-index={@index}
+          >
+            <pre><code class="language-elixir"><%= FormatMarkup.get_preview_text(@gist.markup_text) %></code></pre>
+          </div>
+        </div>
+    </div>
+    """
+  end
+end
